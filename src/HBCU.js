ebd6bb12e3a5990facf93aff7c3d5198c2f9287f
Rod Ross <rodrossdev@gmail.com>
2020-06-19 20:58:46 -0400

    refactoring

--- src/HBCU.js
+++ src/HBCU.js
@@ -1,71 +1,32 @@
 import React from 'react';
+import Cards from './Cards';
 
 class HBCUS extends React.Component {
 
     state = {
         hasErrors: false,
-        articles: {},
-        article1: "",
         articlesAll:[]
     };
 
     componentDidMount() {
-        fetch("https://jervay.agency/hbcus")        
-        // fetch("http://192.168.0.4:8082/Business")        
-            .then(res => res.json())
-            .then(res => {
-                // this.setState({ articles: res })
-                const articles = res.articles;
-                const article1 = articles[1]
-                console.log(articles)
-                console.log(article1)
-                this.setState({
-                    articlesAll: articles,
-                    article1: article1.description,
-                 content:   article1.content
-                })
-                console.log(this.state.article1);
-                // console.log(res.articles[0].title)
-               
+        fetch("https://jervay.agency/hbcus")      
+        .then(res => res.json())
+        .then(res => {          
+            const articles = res.articles;              
+            this.setState({
+                articlesAll: articles,
             })
-            .catch(() => this.setState({ hasErrors: true }))
-        
-        
-        
-    }
+        })
+        .catch(() => this.setState({ hasErrors: true }))
+    
+}
 
-    render() {
-       
-        let arr = [];
-        console.log("arr: ", arr)
-        console.log("render: ", this.state.articlesAll)
-
-        Object.keys(this.state.articlesAll).forEach((key) => arr.push(this.state.articlesAll[key]));
-        return (            
-            // <div>
-                <div className="ui container center aligned">                                      
-                    {/* <div className='ui container'>                         */}
-                <div className="ui stackable three column grid">                   
-                            {arr.map((item, index) =>
-                                <div className="column"  key={index}>
-                                    <div className='ui segment'>
-                                        <a href= {item.url}>
-                                            <img size='small' src={item.urlToImage} alt="Click Me" style={{ width: "100%" }} />
-                                        </a>
-                                        <h3 className='ui header'>
-                                            {item.title}
-                                        </h3>
-                                        <div className="ui divider"></div>
-                                        <div className="column">
-                                            {item.description}
-                                        </div>
-                                    </div>
-                                </div>
-                             )}                           
-                        </div>
-                    {/* </div> */}
-                </div>
-            // </div>           
+render() {      
+    
+    return (   
+            <div>               
+                <Cards>{this.state.articlesAll}</Cards>
+            </div>
         )
     }
 }
